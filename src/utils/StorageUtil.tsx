532c2bfd5fcd66031b798de9d5c21dccb5a4a863
lxcraftcm <lxcraftcm@163.com>
2025-11-25 15:01:47 +0800

    fix: StorageUtil.tsx

--- src/utils/StorageUtil.tsx
+++ src/utils/StorageUtil.tsx
@@ -1,23 +1,11 @@
 export const getLocalStorage = (key: string) => {
-    if (typeof window !== 'undefined') {
-        // 仅在客户端执行
-        return localStorage.getItem(key);
-    }
-    return null;
+    return localStorage.getItem(key);
 };
 
 export const setLocalStorage = (key: string, value: string) => {
-    if (typeof window !== 'undefined') {
-        // 仅在客户端执行
-        return localStorage.setItem(key, value);
-    }
-    return null;
+    return localStorage.setItem(key, value);
 };
 
 export const clearLocalStorage = () => {
-    if (typeof window !== 'undefined') {
-        // 仅在客户端执行
-        return localStorage.clear();
-    }
-    return null;
+    return localStorage.clear();
 };
