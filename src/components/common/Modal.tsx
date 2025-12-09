7fe0971acd74c4e4da239058931b7f6da0918a00
lxcraftcm <lxcraftcm@163.com>
2025-12-09 09:53:01 +0800

    style: i18n Modal impl

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -11,6 +11,7 @@ import {
     faTrash,
     faUndo
 } from "@fortawesome/free-solid-svg-icons";
+import {useTranslations} from 'next-intl';
 
 interface ModalBase {
     visible: boolean;
@@ -39,6 +40,9 @@ const IconMap = {
 };
 
 export const Modal = ({visible, onClose, children, className}: ModalBase) => {
+    // 翻译钩子
+    const t = useTranslations('Modal');
+
     const modalRef = useRef<HTMLDivElement>(null);
     const backdropRef = useRef(null);
     const [container, setContainer]: any = useState(null);
@@ -96,7 +100,7 @@ export const Modal = ({visible, onClose, children, className}: ModalBase) => {
                     onClick={onClose}
                     className={`absolute top-3 right-3 p-1.5 rounded-full transition-colors text-slate-400 hover:bg-slate-100
                      dark:hover:bg-slate-700 hover:text-slate-700 dark:hover:text-slate-300 cursor-pointer`}
-                    aria-label="关闭"
+                    aria-label={t('close')}
                 >
                     <FontAwesomeIcon icon={faXmark} className="h-4 w-4"/>
                 </button>
@@ -112,6 +116,9 @@ export const Modal = ({visible, onClose, children, className}: ModalBase) => {
 
 // 预设消息弹窗
 export const MessageModal = ({visible, onClose, onCancel, onConfirm, type, title, message}: ModalMessage) => {
+    // 翻译钩子
+    const t = useTranslations('Modal');
+
     return (
         <Modal visible={visible} onClose={onClose}>
             <div className="w-100 ">
@@ -131,7 +138,7 @@ export const MessageModal = ({visible, onClose, onCancel, onConfirm, type, title
                                 className="px-4 py-2 rounded-lg text-sm bg-slate-200 dark:bg-slate-700 hover:bg-slate-300 dark:hover:bg-slate-600
                                 transition-colors text-slate-700 dark:text-slate-300 cursor-pointer"
                             >
-                                取消
+                                {t('buttons.cancel')}
                             </button>
                             <button
                                 onClick={onConfirm}
@@ -143,7 +150,7 @@ export const MessageModal = ({visible, onClose, onCancel, onConfirm, type, title
                                             : 'bg-indigo-600 text-white hover:bg-indigo-700'
                                 }`}
                             >
-                                确认
+                                {t('buttons.confirm')}
                             </button>
                         </div>
                     </div>
